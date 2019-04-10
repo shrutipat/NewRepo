7ba43656dff46d7c02a15dd5bef9ee8b27238483
Archita <contactarchitap@gmail.com>
2019-04-10 12:31:11 +0100

    correction on 13

--- aptrial/src/homework_06_04_2019/hasSharedDigit_13.java
+++ aptrial/src/homework_06_04_2019/hasSharedDigit_13.java
@@ -2,8 +2,8 @@ package homework_06_04_2019;
 
 import java.util.Scanner;
 
-public class hasSharedDigit {
-    /*Write a method named hasSharedDigit with two parameters of type int.
+public class hasSharedDigit_13 {
+    /*Write a method named hasSharedDigit_13 with two parameters of type int.
     Each number should be within the range of 10 (inclusive) - 99 (inclusive). If one of the numbers is not within the range, the method should return false.
     The method should return true if there is a digit that appears in both numbers, such as 2 in 12 and 23; otherwise, the method should return false.
     EXAMPLE*/
